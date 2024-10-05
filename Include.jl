4225f31571f11239e6072dfec088f35c1f125aa5
Jeffrey Varner <jdv27@cornell.edu>
2024-10-05 17:34:22 -0400

    PS3 5660 draft almost done
    almost done

--- Include.jl
+++ Include.jl
@@ -2,6 +2,7 @@
 const _ROOT = pwd();
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_SOLN = joinpath(_ROOT, "soln");
 
 # make sure all is up to date -
 using Pkg
@@ -25,6 +26,8 @@ using FileIO
 using Distributions
 using DataStructures
 using PrettyTables
+using Test
 
 # include -
 include(joinpath(_PATH_TO_SRC, "Files.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
---
+++ src/Compute.jl
@@ -0,0 +1,7 @@
+function _nodes(tree::MyBinomialEquityPriceTree, level::Int64)::Array{MyBiomialLatticeEquityNodeModel,1}
+
+    # initialize -
+    return tree.levels[level] .|> x-> tree.data[x]
+end
+
+(tree::MyBinomialEquityPriceTree)(level::Int64)::Array{MyBiomialLatticeEquityNodeModel,1} = _nodes(tree, level)
